1d3765cc6d102e043f26d82deca7e37c822afa20
jmcord <jcorderodz@hotmail.com>
2024-03-21 17:57:08 +0100

    c

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,8 +1,8 @@
 const ChemiCoin = artifacts.require("ChemiCoin");
 
 module.exports = function(deployer) {
-  const name = "ChemiCoin"; // Nombre del token
-  const symbol = "CHEMI"; // Símbolo del token
+  const name = "PigCoin"; // Nombre del token
+  const symbol = "PIG"; // Símbolo del token
   const initialOwner = "0x14d9Cb08D9EC82248f80ce136321e9cbDc4A51a2"; // Dirección del propietario inicial
 
   deployer.deploy(ChemiCoin);
