bd9313e8c4165aa612700d0bbe018cb6239f290a
jmcord <jcorderodz@hotmail.com>
2024-04-04 16:13:42 +0200

    c

---
+++ src/components/NFTs.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import smart_contract_abi from '../abis/ChemiCoin.json';
+import Web3 from 'web3';
+import Swal from 'sweetalert2';
+
+
+import Navigation from './Navbar';
+import MyCarousel from './Carousel';
+import TokenList from './TokenList';
+import { Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+
+
+
+class NFTs extends Component {
+}
+export default NFTs;
