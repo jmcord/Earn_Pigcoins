9558863a112205b7d3f331b893d41008e283a0bf
jmcord <jcorderodz@hotmail.com>
2024-04-16 11:39:28 +0200

    c

--- src/contracts/MyNFT.sol
+++ src/contracts/MyNFT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract BasicNFT is ERC721, ERC721Enumerable, ERC721URIStorage {
+contract MyNFT is ERC721, ERC721Enumerable, ERC721URIStorage {
     using Counters for Counters.Counter;    // Initialize the counter library
 
     // Initialize a counter variable, private means that only this smart contract can access it. 
